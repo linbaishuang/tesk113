a2748c61aab7b2ad46f7cfbed2b371cfe719db30
linbaishuang <1765259718@qq.com>
2017-11-26 16:40:53 +0800

    完成

--- main/main.js
+++ main/main.js
@@ -1,4 +1,23 @@
-module.exports = function main() {
+module.exports.main = function main() {
     console.log("Debug Info");
     return 'Hello World!';
+};
+
+module.exports.allmoney = function allmoney(run, wait) {
+    var money = 0;
+    if(run > 8){
+        money += (run-8) * (0.8+0.8*0.5) + 8*0.8;
+    }
+    else if(run >2 && run <=8 ){
+        money += 0.8*8;
+    }
+    else if(run>0 && run <=2){
+        money += 6;
+    }
+
+    if(wait>0){
+        money += wait * 0.25;
+    }
+
+    console.log(Math.round(money));
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,12 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    var wait = 5;
+    var run = 12;
+    it("money", function () {
+        spyOn(console, 'log');
+
+        main.allmoney(run, wait);
+        expect(console.log).toHaveBeenCalledWith(12);
+    })
 });
